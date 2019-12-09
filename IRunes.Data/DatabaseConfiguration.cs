c8fc3b4a11343344d7bc503f3cb530bcc6ecea98
bobiOne <bobione@abv.bg>
2019-12-09 15:09:46 +0200

    Add project files.

---
+++ IRunes.Data/DatabaseConfiguration.cs
@@ -0,0 +1,8 @@
+﻿namespace IRunes.Data
+{
+    public class DatabaseConfiguration
+    {
+        public const string ConnectionString = "Data Source = (localdb)\\MSSQLLocalDB;Database=RunesDb;" +
+               "Integrated Security=True;Trusted_Connection=True";
+    }
+}
