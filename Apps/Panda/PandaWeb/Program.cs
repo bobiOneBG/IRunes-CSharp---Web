7cbbc3c760be7891905574c85043dac8ea2e68a5
bobiOne <bobione@abv.bg>
2020-01-11 15:03:49 +0200

    create pandaWeb

---
+++ Apps/Panda/PandaWeb/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace PandaWeb
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
